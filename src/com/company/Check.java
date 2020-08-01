a9f1b521e9d9bca8546987607637802c9ae0e0fb
igor_slivka <new.slivka.gmail.com>
2020-08-01 15:04:11 +1000

    my game

---
+++ src/com/company/Check.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Check extends StartGame {
+    String result = null;
+    public String checkWin(){
+
+        return result;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+StartGame game = new StartGame() ;
+game.Toy();
+    }
+}
